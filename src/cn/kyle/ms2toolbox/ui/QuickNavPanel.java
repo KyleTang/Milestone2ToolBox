e620591f24169bd46a697ea2336e9f50a71133b3
Kyle Tang <homects@qq.com>
2011-09-16 02:58:03 +0800

    发布1.7.5 beta3 1. 超频工具箱，可以保存四组自定义配置 2. 长按搜索，勾选M2T为默认，则启动M2T快捷面板 3. 替换blur桌面程序菜单的摩托商店和任务管理器，安装插件后，在工具箱里可以配置

--- src/cn/kyle/ms2toolbox/ui/QuickNavPanel.java
+++ src/cn/kyle/ms2toolbox/ui/QuickNavPanel.java
@@ -2,6 +2,8 @@ package cn.kyle.ms2toolbox.ui;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 import cn.kyle.ms2toolbox.Event;
 import cn.kyle.ms2toolbox.Pref;
@@ -12,14 +14,25 @@ import cn.kyle.util.L;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
@@ -30,6 +43,7 @@ import android.widget.Toast;
 
 public class QuickNavPanel extends Activity {
 	Toast myToast = null;
+	LinearLayout quickNavPanelList ;
 	/**
 	 * @see android.app.Activity#onCreate(Bundle)
 	 */
@@ -38,7 +52,135 @@ public class QuickNavPanel extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.quicknavpanel );
 		Event.count(this, Event.QuickNavPanel);
-		myToast("Oops. comming soon.");
+		quickNavPanelList = (LinearLayout)findViewById(R.id.quickNavPanelList);
+		addSearchItems();
+		addMs2ToolBoxItems();
+		//myToast("Oops. comming soon.");
+	}
+	
+	public void addSearchItems(){
+		PackageManager pm = getPackageManager();
+    	List<ResolveInfo> activities = pm.queryIntentActivities(
+    		new Intent(Intent.ACTION_SEARCH_LONG_PRESS), 0);
+    	L.debug("--begin");
+    	if (activities.size()>0){
+    		Iterator<ResolveInfo> it = activities.iterator();
+    		ResolveInfo ri = null;
+    		while(it.hasNext()){
+    			ri = it.next();
+    			if ("cn.kyle.ms2toolbox".equals(ri.activityInfo.packageName)){
+    				continue;
+    			}
+    			L.debug("activity: "+ri.activityInfo.packageName+", "+ri.activityInfo.toString());
+    			Button btn = new Button(this);
+    	    	btn.setWidth(32);
+    	    	btn.setHeight(32);
+    	    	final ActivityInfo ai = ri.activityInfo;
+    	    	Drawable d = ai.loadIcon(pm);
+    	    	btn.setBackgroundDrawable(ai.loadIcon(pm));
+    	    	btn.setOnClickListener(new OnClickListener(){
+    				public void onClick(View v) {
+    					Intent i = new Intent();
+    					i.setAction(Intent.ACTION_MAIN);
+    					i.setComponent(new ComponentName(ai.packageName, ai.name));
+    					startActivity(i);
+    					QuickNavPanel.this.finish();
+    				}
+    	    	});
+    	    	btn.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT , LayoutParams.WRAP_CONTENT));
+    	    	LinearLayout llTmp = new LinearLayout(this);
+    	    	llTmp.setOrientation(LinearLayout.HORIZONTAL);
+    	    	llTmp.setHorizontalScrollBarEnabled(false);
+    	    	llTmp.setHorizontalScrollBarEnabled(true);
+    	    	llTmp.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT , LayoutParams.WRAP_CONTENT));
+    	    	llTmp.addView(btn);
+    	    	llTmp.setBackgroundColor(android.R.color.background_dark);
+    	    	TextView tvBtnTip = new TextView(this);
+    	    	//tvBtnTip.setTextColor(android.R.color.white);
+    	    	try {
+					tvBtnTip.setText(pm.getApplicationLabel(pm.getApplicationInfo(ai.packageName, PackageManager.GET_META_DATA)));
+				} catch (NameNotFoundException e) {
+					e.printStackTrace();
+				}
+    	    	llTmp.addView(tvBtnTip);
+    	    	quickNavPanelList.addView(llTmp);
+    		}
+    	}
+	}
+	
+	public void addMs2ToolBoxItems(){
+		final String packageName = "cn.kyle.ms2toolbox";
+    	LinearLayout llTmp = new LinearLayout(this);
+    	llTmp.setOrientation(LinearLayout.HORIZONTAL);
+    	llTmp.setHorizontalScrollBarEnabled(false);
+    	llTmp.setHorizontalScrollBarEnabled(true);
+    	llTmp.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT , LayoutParams.WRAP_CONTENT));
+    	llTmp.setBackgroundColor(android.R.color.primary_text_dark);
+    	
+    	//锁屏
+		Button btn1 = new Button(this);
+     	btn1.setBackgroundResource(R.drawable.lockscreen);
+    	btn1.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT , LayoutParams.WRAP_CONTENT));
+    	btn1.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				Intent i = new Intent(QuickNavPanel.this,LockScreen.class);
+				startActivity(i);
+				QuickNavPanel.this.finish();
+			}
+    	});
+    	
+    	//手电
+    	Button btn2 = new Button(this);
+     	btn2.setBackgroundResource(R.drawable.flashlight);
+    	btn2.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT , LayoutParams.WRAP_CONTENT));
+    	btn2.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				Intent i = new Intent(QuickNavPanel.this,FlashLight.class);
+				startActivity(i);
+				QuickNavPanel.this.finish();
+			}
+    	});
+    	
+    	//亮度调节
+    	Button btn3 = new Button(this);
+    	//btn3.setBackgroundDrawable(getDrawableByResid(R.drawable.lcdbacklight));
+    	btn3.setBackgroundResource(R.drawable.lcdbacklight);
+    	btn3.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT , LayoutParams.WRAP_CONTENT));
+    	btn3.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				Intent i = new Intent(QuickNavPanel.this,LcdBackLight.class);
+				startActivity(i);
+				QuickNavPanel.this.finish();
+			}
+    	});
+    	
+    	//电源
+    	Button btn4 = new Button(this);
+    	//btn4.setBackgroundDrawable(getDrawableByResid(R.drawable.power));
+    	btn4.setBackgroundResource(R.drawable.power);
+    	btn4.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT , LayoutParams.WRAP_CONTENT));
+    	btn4.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				Intent i = new Intent(QuickNavPanel.this,Power.class);
+				startActivity(i);
+				QuickNavPanel.this.finish();
+			}
+    	});
+    	llTmp.addView(btn1);
+    	llTmp.addView(btn2);
+    	llTmp.addView(btn3);
+    	llTmp.addView(btn4);
+    	
+    	quickNavPanelList.addView(llTmp);
+	}
+	
+	public Drawable getDrawableByResid(int id){
+    	Matrix matrix = new Matrix();
+        matrix.postScale(72, 72);
+    	Bitmap bitmap = Bitmap.createBitmap(
+    			BitmapFactory.decodeResource(this.getResources(), id)
+    			,0,0,128,128,matrix,true);
+    	return new BitmapDrawable(this.getResources(), bitmap);
 	}
 	
 	public void myToast(String tipInfo) {
